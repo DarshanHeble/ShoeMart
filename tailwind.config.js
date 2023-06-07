3d9029b706205946d64524f929c7e1be70af89a3
DarshanHeble <109078053+DarshanHeble@users.noreply.github.com>
2023-06-07 20:11:52 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./*.{html,js}"],
+module.exports = {
+  content: ["*"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
